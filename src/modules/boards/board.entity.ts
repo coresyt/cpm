307b11f8a762add1b5c73b00594c14bb999e0200
coresyt <corejsyt2011@gmail.com>
2026-08-04 09:41:44 -0600

    feat: added the "origin" column to the board entity

--- src/modules/boards/board.entity.ts
+++ src/modules/boards/board.entity.ts
@@ -1,3 +1,4 @@
+import { IsMACAddress } from 'class-validator'
 import {
   Column,
   CreateDateColumn,
@@ -15,6 +16,10 @@ export class Board {
 
   @Column('uuid')
   userId: string
+
+  @Column({ type: 'varchar', default: '' })
+  @IsMACAddress({ eui: '48' })
+  origin: string
 
   @Column({ type: 'simple-array', default: [] })
   thingsIds: string[]
