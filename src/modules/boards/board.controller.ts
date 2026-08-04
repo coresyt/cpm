49a6a452072e354dba79f05d328303cde986d6b0
coresyt <corejsyt2011@gmail.com>
2026-08-04 10:04:17 -0600

    feat: added routes and controllers to add createTokenBoard to the boards

--- src/modules/boards/board.controller.ts
+++ src/modules/boards/board.controller.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from 'express'
 import AppDataSource from '../../shared/database/database'
 import { Board } from './board.entity'
 import { isIP } from 'class-validator'
+import { createToken } from '../../shared/utils/jwt.util'
 
 export const listBoards: RequestHandler = async (req, res) => {
   try {
@@ -93,6 +94,37 @@ export const createBoard: RequestHandler = async (req, res) => {
     console.log(err)
     res.status(500).json({ status: 500, message: 'Internal server error' })
   }
+}
+
+export const createTokenBoard: RequestHandler = async (req, res) => {
+  if (typeof req.user !== 'object' || req.verifyIdentity !== true) {
+    res.status(404).json({
+      status: 404,
+      message: 'Not found!'
+    })
+    return
+  }
+
+  if (req.params.id === undefined) {
+    return res.status(400).json({
+      status: 400,
+      message: 'Bad request!'
+    })
+  }
+
+  const tokenBoard = await createToken({
+    userId: req.user.id,
+    role: req.user.role,
+    boardId: req.params.id
+  })
+
+  res
+    .status(200)
+    .json({
+      status: 200,
+      message: 'Created board token successfully!',
+      token: tokenBoard
+    })
 }
 
 export const updateBoard: RequestHandler = async (req, res) => {
