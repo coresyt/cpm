ac4f1c685446b0c722fcc629ef8d3234ab6976bb
coresyt <corejsyt2011@gmail.com>
2026-08-05 22:56:25 -0600

    feat(frontend): create the signup form

--- apps/frontend/src/app.d.ts
+++ apps/frontend/src/app.d.ts
@@ -1,6 +1,12 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
+	interface LocalStorageSession {
+		token: string;
+		name?: string;
+		email: string;
+	}
+
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
