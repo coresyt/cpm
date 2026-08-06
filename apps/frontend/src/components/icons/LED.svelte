30d65225613f60fe0bfc339837853b051b169249
coresyt <corejsyt2011@gmail.com>
2026-08-06 11:13:18 -0600

    feat(frontend): added CPU and LED icon

---
+++ apps/frontend/src/components/icons/LED.svelte
@@ -0,0 +1,6 @@
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 32 32"
+	><path
+		fill="currentColor"
+		d="M16 2c4.963 0 9 4.037 9 9v8h1a1 1 0 1 1 0 2h-5.941v5a1 1 0 1 1-2 0v-5H13.94v8a1 1 0 1 1-2 0v-8H6a1 1 0 1 1 0-2h1v-8c0-4.963 4.037-9 9-9m0 2c-3.86 0-7 3.14-7 7v8h14v-8c0-3.86-3.14-7-7-7m-3 4a1 1 0 0 1 1 1v7a1 1 0 1 1-2 0V9a1 1 0 0 1 1-1"
+	></path></svg
+>
