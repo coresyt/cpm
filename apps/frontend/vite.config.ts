ae26472624cdc73d29eeb197625227282cb1331c
coresyt <corejsyt2011@gmail.com>
2026-08-05 20:05:21 -0600

    feat(frontend): added icons

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -15,6 +15,9 @@ export default defineConfig({
 				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
 
+			alias: {
+				"$components": 'src/components/*'
+			},
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
